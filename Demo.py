1cb329c8c526d3d2b6d1336204b5c1ab9f520d03
Gourav2718 <118345825+Gourav2718@users.noreply.github.com>
2025-02-11 09:12:05 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+a,b = b,a
+print("After Swapping First Number is:",a)
+print("After Swapping Second Number is:",b)
